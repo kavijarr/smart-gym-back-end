bc7074d5def3c23184b2475b0ff7f2121918cf73
kavijarr <kavijakumuditha12@gmail.com>
2024-06-08 09:02:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# smart-gym-front-end
---
+++ src/main/java/org/example/entity/CustomerEntity.java
@@ -0,0 +1,15 @@
+package org.example.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+public class CustomerEntity {
+}
